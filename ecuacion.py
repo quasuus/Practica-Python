34924978ce6b2191f3a6de886d411cfa5d8f2841
quasuus <andrea.garcia2026@hilasal.superate.org.sv>
2025-02-18 20:55:27 -0600

    Actividad terminada :tada:

---
+++ ecuacion.py
@@ -0,0 +1,6 @@
+def ecuacion_realizar(x):
+    return (x * 3 + 5)/2
+
+x = int(input("Coloca un número para asignarle a x aqui: "))
+result = ecuacion_realizar(x)
+print(f"El resultado de la ecuacion es: {result}")
